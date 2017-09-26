d22a4ecccab786b70e93f0231a20a49f7822b1ca
Jenny <hello@jennygunnarsson.com>
2017-09-26 14:48:26 +0200

    looping cards

--- src/Game.js
+++ src/Game.js
@@ -2,24 +2,44 @@ import React from "react"
 import Card from "./Card"
 import Counter from "./Counter"
 
-const Game = () => (
-  <div>
-    <header className="game-header">
-      <h1>Where is the Bunny?</h1>
-    </header>
-    <div className="card-container">
-      <Card src="/images/bunny1.jpg" />
-      <Card src="/images/bunny2.jpg" />
-      <Card src="/images/bunny3.jpg" />
-      <Card src="/images/bunny1.jpg" />
-      <Card src="/images/bunny2.jpg" />
-      <Card src="/images/bunny5.jpg" />
-      <Card src="/images/bunny3.jpg" />
-      <Card src="/images/bunny4.jpg" />
-      <Card src="/images/bunny4.jpg" />
-      <Card src="/images/bunny5.jpg" />
-    </div>
-  </div>
-)
+class Game extends React.Component {
+
+  state = {
+    cards: [
+      {
+        src: "/images/bunny1.jpg"
+      },
+      {
+        src: "/images/bunny2.jpg"
+      },
+      {
+        src: "/images/bunny3.jpg"
+      },
+      {
+        src: "/images/bunny4.jpg"
+      },
+      {
+        src: "/images/bunny5.jpg"
+      }
+    ]
+  }
+
+  render() {
+
+    return (
+      <div>
+        <header className="game-header">
+          <h1>Where is the Bunny?</h1>
+        </header>
+
+        <div className="card-container">
+          {this.state.cards.map((card) => (
+            <Card src={card.src} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Game
